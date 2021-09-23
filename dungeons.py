8514db7e7757c72fd8491e2ec4955e0abe08b267
Leonardo Felipe <86111062+leonardofmoraes@users.noreply.github.com>
2021-09-22 22:42:45 -0300

    Create dungeons.py
    InÍcio do projeto N1.

---
+++ dungeons.py
@@ -0,0 +1,3 @@
+print("Olá! Seja bem-vindo ao Dungeons - Grupo 5.\n")
+Guilda = (input("Digite o nome da sua Guilda:\n"))
+print("A guilda " + Guilda + " começa na sala 1 e precisa chegar na sala 9 podendo escolher somente dois caminhos.\n")
